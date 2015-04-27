bce7671da600b05266042b53ca781023222ef66b
Jesper <karnich99@gmail.com>
2015-04-27 20:03:45 +0200

    json deserilization fra nettet nu!

--- DAL/JSONDeserialisator.cs
+++ DAL/JSONDeserialisator.cs
@@ -14,72 +14,42 @@ using Newtonsoft.Json.Linq;
 
 namespace DAL
 {
-
-
     public class JSONDeserialisator
     {
 
-        public Tuple<List<Appartment>, List<Sensor>> DeserialiseOriginalFile(string filepath)
+        public Tuple<List<Appartment>, List<Sensor>> DeserialiseOriginalFile(string json)
         {
-            using (StreamReader r = new StreamReader(filepath))
-            {
-                string json = r.ReadToEnd();
-                int appartmentBegin = json.IndexOf("[", 0);
-                int appartmentEnd = json.IndexOf("]", appartmentBegin);
-                int sensorBegin = json.IndexOf("[", appartmentEnd);
-                int sensorEnd = json.IndexOf("]", sensorBegin);
+            int appartmentBegin = json.IndexOf("[", 0);
+            int appartmentEnd = json.IndexOf("]", appartmentBegin);
+            int sensorBegin = json.IndexOf("[", appartmentEnd);
+            int sensorEnd = json.IndexOf("]", sensorBegin);
 
-                string appartments = json.Substring(appartmentBegin, appartmentEnd-appartmentBegin+1);
-                List<Appartment> appList = JsonConvert.DeserializeObject<List<Appartment>>(appartments);
-                int len = json.Length;
-                string sensors = json.Substring(sensorBegin, sensorEnd-sensorBegin+1);
-                List<Sensor> senList = JsonConvert.DeserializeObject<List<Sensor>>(sensors);
+            string appartments = json.Substring(appartmentBegin, appartmentEnd-appartmentBegin+1);
+            List<Appartment> appList = JsonConvert.DeserializeObject<List<Appartment>>(appartments);
+            int len = json.Length;
+            string sensors = json.Substring(sensorBegin, sensorEnd-sensorBegin+1);
+            List<Sensor> senList = JsonConvert.DeserializeObject<List<Sensor>>(sensors);
 
-                return new Tuple<List<Appartment>, List<Sensor>>(appList, senList);
-            }
+            return new Tuple<List<Appartment>, List<Sensor>>(appList, senList);
+
         }
 
-        public List<Measurement> DeserialiseMeasurement(string filepath)
+        public List<Measurement> DeserialiseMeasurement(string json)
         {
-            using (StreamReader r = new StreamReader(filepath))
-            {
-                string json = r.ReadToEnd();
-                int begin = json.IndexOf("[", 0);
-                int end = json.IndexOf("]", begin);
-                string o = json.Substring(begin, end-begin+1);
-                return JsonConvert.DeserializeObject<List<Measurement>>(o);
-            }
-        }
-
-
-        public class Measurement
-        {
-            public double Value { get; set; }
-            public string Timestamp { get; set; }
-
-            public Sensor Sensor { get; set; }
-            public int SensorId { get; set; }
-            public Appartment Appartment { get; set; }
-            public int AppartmentId { get; set; }
-        }
-
-        public class Appartment
-        {
-            public int AppartmentId { get; set; }
-            public int Floor { get; set; }
-            public int Number { get; set; }
-            public double Size { get; set; }
-        }
-
-        public class Sensor
-        {
-            public int SensorId { get; set; }
-            public string CalibrationCoeff { get; set; }
-            public string Description { get; set; }
-            public string CalibrationDate { get; set; }
-            public string ExternalRef { get; set; }
-            public string Unit { get; set; }
-            public string CalibrationEquation { get; set; }
+            int begin = json.IndexOf("[", 0);
+            int end = json.IndexOf("]", begin);
+            string o = json.Substring(begin, end-begin+1);
+            return JsonConvert.DeserializeObject<List<Measurement>>(o);
         }
     }
 }
+
+//old method
+//            using (StreamReader r = new StreamReader(filepath))
+//            {
+//                string json = r.ReadToEnd();
+//                int begin = json.IndexOf("[", 0);
+//                int end = json.IndexOf("]", begin);
+//                string o = json.Substring(begin, end-begin+1);
+//                return JsonConvert.DeserializeObject<List<Measurement>>(o);
+//            }
---
+++ DAL/StringDownloader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public static class StringDownloader
+    {
+        public static string DownloadStringFromURL(string url)
+        {
+            string json;
+            using (var webClient = new System.Net.WebClient())
+            {
+                json = webClient.DownloadString(url);
+            }
+            return json;
+        }
+
+    }
+}
