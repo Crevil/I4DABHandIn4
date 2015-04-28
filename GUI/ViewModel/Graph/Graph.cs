4e8b7a32d8c88f32e4bbcf608053b89e052db7c3
Crevil <bjoern.soerensen@gmail.com>
2015-04-28 18:30:10 +0200

    Missed some files

---
+++ GUI/ViewModel/Graph/Graph.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using GUI.Annotations;
+using GUI.ViewModel.Graph.Types;
+using OxyPlot;
+
+namespace GUI.ViewModel.Graph
+{
+    public class Graph : INotifyPropertyChanged
+    {
+        private PlotModel _plotModel;
+        private readonly IGraphType _type;
+
+        public PlotModel PlotModel
+        {
+            get { return _plotModel; }
+            set { _plotModel = value; OnPropertyChanged(); }
+        }
+
+        public Graph(IDataProvider dataProvider)
+        {
+            PlotModel = new PlotModel();
+            _type = new TemperatureGraph {PlotModel = PlotModel, DataProvider = dataProvider};
+            _type.SetUpModel();
+            _type.LoadData();
+        }
+
+        public void UpdateModel() { _type.UpdateModel(); }
+
+        #region Plot markup
+        public static readonly List<OxyColor> Colors = new List<OxyColor>
+                                            {
+                                                OxyColors.Green,
+                                                OxyColors.IndianRed,
+                                                OxyColors.Coral,
+                                                OxyColors.Chartreuse,
+                                                OxyColors.Azure
+                                            };
+
+        public static readonly List<MarkerType> MarkerTypes = new List<MarkerType>
+                                                   {
+                                                       MarkerType.Plus,
+                                                       MarkerType.Star,
+                                                       MarkerType.Diamond,
+                                                       MarkerType.Triangle,
+                                                       MarkerType.Cross
+                                                   };
+        #endregion // Plot markup
+
+        #region PropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion // PropertyChanged
+    }
+}
---
+++ GUI/ViewModel/Sensors.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using DAL.Entities;
+
+namespace GUI.ViewModel
+{
+    public class Sensors : ObservableCollection<Sensor>, INotifyPropertyChanged
+    {
+        public Sensors()
+        {
+            Add(new Sensor { Description = "Sensor Test1" });
+            Add(new Sensor { Description = "Sensor Test2" });
+            Add(new Sensor { Description = "Sensor Test3" });
+
+        }
+    }
+}
