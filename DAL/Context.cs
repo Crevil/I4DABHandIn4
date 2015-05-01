0360d89729484bef8783fb2e0adc09aaef299145
Thisen <thisen-dk@hotmail.com>
2015-05-01 21:26:04 +0200

    Changed context to IHAs database

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -11,7 +11,7 @@ namespace DAL
 {
     public class Context : DbContext
     {
-        public Context() : base("GrundfosDormitoryDb")
+        public Context() : base("GrundfosDormitoryDbIHA")
         {
             
         }
